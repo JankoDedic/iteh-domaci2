f19b497259020cf6a36a77ba2bccd42da430fd5f
Janko Dedic <jankodedic7@gmail.com>
2021-12-11 20:23:55 +0100

    Add seeding for users and products

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\Product;
+use App\Models\User;
+use App\Models\WishlistItem;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Schema;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +17,17 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        Schema::disableForeignKeyConstraints();
+
+        WishlistItem::truncate();
+        Product::truncate();
+        User::truncate();
+
+        User::factory()
+            ->count(5)
+            ->has(Product::factory()->count(2))
+            ->create();
+
+        Schema::enableForeignKeyConstraints();
     }
 }
