5eac6121cffb312a63eeaced7b41c72c35fd9dbd
Janko Dedic <jankodedic7@gmail.com>
2021-12-11 18:56:11 +0100

    Implement ProductFactory

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Collection;
 
 class ProductFactory extends Factory
 {
@@ -14,7 +15,34 @@ class ProductFactory extends Factory
     public function definition()
     {
         return [
-            //
+            'manufacturer' => Collection::make([
+                'Samsung',
+                'Apple',
+                'Dell',
+                'Xiaomi',
+                'Toshiba',
+                'Huawei',
+                'Google',
+                'HP',
+                'Microsoft',
+                'Lenovo',
+            ])->random(),
+
+            'name' => Collection::make([
+                'smartphone',
+                'tablet',
+                'laptop',
+                'PC',
+                'monitor',
+                'keyboard',
+                'mouse',
+                'TV',
+                'headphones',
+                'earphones',
+                'power adapter',
+            ])->random(),
+
+            'price' => rand(300, 1200),
         ];
     }
 }
