cc3b29e3e35d825ef1c11241fb314ca3dbc70b63
Janko Dedic <jankodedic7@gmail.com>
2021-12-11 16:49:01 +0100

    Add more columns to the 'products' table

---
+++ database/migrations/2021_12_11_153613_add_more_columns_to_products_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddMoreColumnsToProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->string('manufacturer');
+            $table->string('name');
+            $table->integer('price');
+            $table->foreignId('user_id')->constrained();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->dropColumn('user_id');
+            $table->dropColumn('price');
+            $table->dropColumn('name');
+            $table->dropColumn('manufacturer');
+        });
+    }
+}
