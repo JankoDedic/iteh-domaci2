fa167423a4a2e42990ddd75242f3db3b0f751967
Janko Dedic <jankodedic7@gmail.com>
2021-12-12 16:25:33 +0100

    Add endpoint for getting all products

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,9 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    //
+    public function products(Request $request)
+    {
+        return Product::all();
+    }
 }
