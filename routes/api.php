d1d27676bd10414552a0b622617db6409ad40a24
Janko Dedic <jankodedic7@gmail.com>
2021-12-12 16:36:22 +0100

    Require authentication for the /products route

--- routes/api.php
+++ routes/api.php
@@ -23,4 +23,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
-Route::get('/products', [ProductController::class, 'products']);
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::get('/products', [ProductController::class, 'products']);
+});
